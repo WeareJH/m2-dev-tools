65dab2605db47313bd99ded758cda54f91251579
Shane Osbourne <shane.osbourne8@gmail.com>
2018-03-08 13:28:37 +0000

    add types + real data

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,46 +1,64 @@
+declare var require;
 import {h, Component} from 'preact';
+import {NodeItem} from "../types";
 
 const classnames = require('classnames');
-const node = require('../../fixtures/dump.json');
+const nodes = require('../../fixtures/large.json');
 
-export class App extends Component<any, any> {
+const root: NodeItem = {
+    name: "$$root",
+    children: nodes,
+    data: {type: "root"}
+};
+
+export class App extends Component<any, {selected: Set<string>}> {
 
     state = {
-        selected: new Set([])
+        selected: new Set<string>([])
     }
 
     render() {
         return (
-            <Node
-                node={node}
-                depth={1}
-                selected={this.state.selected}
-                addHover={(label) => {
-                    this.setState(prev => ({
-                        selected: (prev.selected.add(label), prev.selected)
-                    }))
-                }}
-                removeHover={(label) => {
-                    this.setState(prev => ({
-                        selected: (prev.selected.delete(label), prev.selected)
-                    }))
-                }}
-            />
+            <div class="node-tree">
+                <Node
+                    node={root}
+                    depth={1}
+                    selected={this.state.selected}
+                    addHover={(label) => {
+                        this.setState(prev => ({
+                            selected: (prev.selected.add(label), prev.selected)
+                        }))
+                    }}
+                    removeHover={(label) => {
+                        this.setState(prev => ({
+                            selected: (prev.selected.delete(label), prev.selected)
+                        }))
+                    }}
+                />
+            </div>
         )
     }
 }
 
-export function Node({node, depth, selected, addHover, removeHover}) {
-    const {nodes} = node;
-    const hasNodes = nodes && (nodes.length > 0);
+export interface NodeProps {
+    node: NodeItem,
+    depth: number,
+    selected: Set<string>,
+    addHover(name: string): void,
+    removeHover(name: string): void
+}
+export function Node(props: NodeProps) {
+    const {node, depth, addHover, removeHover, selected} = props;
+    const {children} = node;
+    const hasNodes = children && (children.length > 0);
     const body = (hasNodes) && (
         <div class="nodes">
-            {nodes.map(n => {
+            {children.map(n => {
                 const nextDepth = depth + 1;
                 return <Node
                     node={n}
                     depth={nextDepth}
-                    key={n.label}
+                    key={n.name}
                     selected={selected}
                     addHover={addHover}
                     removeHover={removeHover}
@@ -49,9 +67,10 @@ export function Node({node, depth, selected, addHover, removeHover}) {
         </div>
     );
     const indent = depth * 15;
-    const isSelected = selected.has(node.label);
+    const isSelected = selected.has(node.name);
     const head = <NodeInfo
         node={node}
+        hasChildren={hasNodes}
         indent={indent}
         addHover={addHover}
         removeHover={removeHover}
@@ -73,49 +92,56 @@ export function Node({node, depth, selected, addHover, removeHover}) {
         </div>
     );
 }
-
-class NodeInfo extends Component<any, any> {
-    state = {
-        hovered: false
-    };
-
-    render() {
-        const {node, indent, addHover, removeHover} = this.props;
-        const classes = classnames({
-            node_info: true,
-            'node_info--hovered': this.props.isSelected
-        });
-        return (
-            <div style={{paddingLeft: String(indent) + 'px'}}
-                 class={classes}
-                 onmouseleave={() => removeHover(node.label)}
-                 onmouseenter={() => addHover(node.label)}>
-                <p>
-                    <span class="token lt">&lt;</span>
-                    <span class="token">{node.label}</span>
-                    <NodeAttr data={node.data} dataKey={'template_file'} attrName={'template'}/>
-                    <span class="token gt">&gt;</span>
-                </p>
-                <div class="node__props">
-                    {node.data && Object.keys(node.data).map(key => {
-                        const value = node.data[key];
-                        if (typeof value === 'string') {
-                            return (
-                                <p>
-                                    <span class="token token--key">{key}:</span>
-                                    {' '}
-                                    <span class="token token--value">{node.data[key]}</span>
-                                </p>
-                            )
-                        }
-                    })}
-                </div>
+export interface NodeInfoProps {
+    node: NodeItem,
+    indent: number,
+    isSelected: boolean,
+    hasChildren: boolean,
+    addHover(name: string): void,
+    removeHover(name: string): void
+}
+function NodeInfo(props: NodeInfoProps) {
+    const {node, indent, addHover, removeHover, hasChildren} = props;
+    const classes = classnames({
+        node_info: true,
+        'node_info--hovered': this.props.isSelected
+    });
+    const nodeName = node.data && node.data.type;
+    return (
+        <div style={{paddingLeft: String(indent) + 'px'}}
+             class={classes}
+             onMouseLeave={() => removeHover(node.name)}
+             onMouseEnter={() => addHover(node.name)}>
+            <p>
+                <span class="token lt">&lt;</span>
+                <span class="token">{nodeName}</span>
+                <NodeAttr data={node.data} dataKey={'name'} attrName={'name'}/>
+                <NodeAttr data={node.data} dataKey={'template_file'} attrName={'template'}/>
+                {!hasChildren && (
+                    <span class="token gt">{' /'}</span>
+                )}
+                <span class="token gt">&gt;</span>
+            </p>
+            <div class="node__props">
+                {node.data && Object.keys(node.data).map(key => {
+                    const value = node.data[key];
+                    if (typeof value === 'string') {
+                        return (
+                            <p>
+                                <span class="token token--key">{key}:</span>
+                                {' '}
+                                <span class="token token--value">{node.data[key]}</span>
+                            </p>
+                        )
+                    }
+                })}
             </div>
-        )
-    }
+        </div>
+    )
 }
 
-function NodeAttr({data, attrName, dataKey}) {
+function NodeAttr(props: {data: any, attrName: string, dataKey: string}) {
+    const {data, attrName, dataKey} = props;
     if (!data) return null;
     if (!data[dataKey]) return null;
     return (
@@ -128,7 +154,16 @@ function NodeAttr({data, attrName, dataKey}) {
     );
 }
 
-function NodeEnd({node, hasChildren, indent, isSelected, addHover, removeHover}) {
+export interface NodeEndProps {
+    node: NodeItem,
+    indent: number,
+    addHover(name: string): void,
+    removeHover(name: string): void
+    hasChildren: boolean
+    isSelected: boolean
+}
+function NodeEnd(props: NodeEndProps) {
+    const {node, hasChildren, indent, isSelected, addHover, removeHover} = props;
     const classes = classnames({
         node_info: true,
         'node_info--hovered': isSelected
@@ -137,9 +172,11 @@ function NodeEnd({node, hasChildren, indent, isSelected, addHover, removeHover})
         <div
             class={classes}
             style={{paddingLeft: String(indent) + 'px'}}
-            onmouseleave={() => removeHover(node.label)}
-            onmouseenter={() => addHover(node.label)}>
-            <span class="token lt">&lt;</span><span class="token">/{node.label}</span><span class="token gt">&gt;</span>
+            onMouseLeave={() => removeHover(node.name)}
+            onMouseEnter={() => addHover(node.name)}>
+            <span class="token lt">&lt;</span>
+            <span class="token">/{node.name}</span>
+            <span class="token gt">&gt;</span>
         </div>
     )
 }
---
+++ src/types.ts
@@ -0,0 +1,20 @@
+export interface NodeItem {
+    name: string
+    json?: string
+    data?: INodeData
+    children: NodeItem[]
+}
+
+interface INodeData {
+    name: string;
+    type: string;
+    block_type: string;
+    template: string;
+    template_file: string;
+    args: IArgs;
+}
+
+interface IArgs {
+    type: string;
+    module_name: string;
+}
