a725d5126c4506f0c1c723c4c62a578b3b4a6ea9
Shane Osbourne <shane.osbourne8@gmail.com>
2018-03-18 19:15:43 +0000

    Styling for selection overlay

--- src/components/SelectionOverlay.tsx
+++ src/components/SelectionOverlay.tsx
@@ -10,14 +10,23 @@ export function SelectionOverlay(props: SelectionOverlayProps) {
     if (!props.visible) {
         return null;
     }
+    const item = props.getItem();
     return (
         <div className="info-window">
-            <button
-                type="button"
-                onClick={() => props.toggleSelectionOverlay(false)}
-            >
-                Close
-            </button>
+            <div className="info-window__controls">
+                <button
+                    type="button"
+                    onClick={() => props.toggleSelectionOverlay(false)}
+                >
+                    Close
+                </button>
+            </div>
+            <div className="info-window__path">
+                {item.node.namePath.map((x) => (
+                        <span className="info-window__segment" key={x}>{x}</span>
+                    )
+                )}
+            </div>
             <pre><code>codex here</code></pre>
         </div>
     )
