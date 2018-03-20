d4ec7024614ca38fcc0b2dd0e8f81b2260281163
Shane Osbourne <shane.osbourne8@gmail.com>
2018-03-20 19:51:43 +0000

    more responsive styles for action bar

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -36,18 +36,20 @@ export function ActionBar(props: ActionBarProps) {
                 getItem={() => pullData(props.root.children, props.flatNodes, selectedId)}
             />
             <div className="controls">
-                <button
-                    type="button"
-                    className="controls__button"
-                    onClick={props.expandAll}
-                >Expand all
-                </button>
-                <button
-                    type="button"
-                    className="controls__button"
-                    onClick={props.collapseAll}
-                >Collapse all
-                </button>
+                <div className="controls__buttons">
+                    <button
+                        type="button"
+                        className="controls__button"
+                        onClick={props.expandAll}
+                    >Expand all
+                    </button>
+                    <button
+                        type="button"
+                        className="controls__button"
+                        onClick={props.collapseAll}
+                    >Collapse all
+                    </button>
+                </div>
                 {/*
                 <button
                     type="button"
@@ -55,37 +57,40 @@ export function ActionBar(props: ActionBarProps) {
                     onClick={props.toggleInspecting}
                 >{props.inspecting ? 'Stop inspecting' : 'Inspect page'}</button>
                 */}
-                <label htmlFor="strip-comments">
-                    <input
-                        type="checkbox"
-                        id="strip-comments"
-                        checked={props.stripComments}
-                        onChange={(e) => {
-                            const checked = e.target.checked;
-                            props.toggleStripComments(checked);
-                        }}
-                    />
-                    Strip Comments
-                </label>
-                <label htmlFor="check">
-                    <input
-                        type="checkbox"
-                        id="check"
-                        checked={props.selectionOverlay}
-                        onChange={(e) => {
-                            const checked = e.target.checked;
-                            props.toggleSelectionOverlay(checked);
-                        }}
-                    />
-                    Selection Overlay
-                </label>
+                <div className="controls__checkboxes">
+                    <label htmlFor="strip-comments" className="controls__checkbox">
+                        <input
+                            type="checkbox"
+                            id="strip-comments"
+                            checked={props.stripComments}
+                            onChange={(e) => {
+                                const checked = e.target.checked;
+                                props.toggleStripComments(checked);
+                            }}
+                        />
+                        Strip Comments
+                    </label>
+                    <label htmlFor="check" className="controls__checkbox">
+                        <input
+                            type="checkbox"
+                            id="check"
+                            checked={props.selectionOverlay}
+                            onChange={(e) => {
+                                const checked = e.target.checked;
+                                props.toggleSelectionOverlay(checked);
+                            }}
+                        />
+                        Selection Overlay
+                    </label>
+                </div>
             </div>
             <div className="search-bar">
-                <label htmlFor="search" className="search-bar__label">Search</label>
                 <input
                     type="text"
                     id="search"
+                    placeholder="search for Block, Container, or Template names"
                     value={props.searchTerm}
+                    className="search-bar__input"
                     onChange={(e) => props.setSearchTerm(e.target.value)}
                 />
             </div>
